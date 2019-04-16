0ed08f74c26bbae4ca9721c1f2ef76ed5c2d7dfb
jleu64 <43893485+jleu64@users.noreply.github.com>
2019-04-16 17:38:34 -0400

    Update UBPacket.cpp
    There are some errors in compliation however this is the design of the packets

--- agent/UBPacket.cpp
+++ agent/UBPacket.cpp
@@ -1,5 +1,4 @@
 #include "UBPacket.h"
-
 UBPacket::UBPacket() : m_srcID(0),
     m_desID(0)
 {
@@ -12,10 +11,43 @@ QByteArray UBPacket::packetize(void) {
 
     return src + des + m_payload;
 }
+QByteArray UBPacket::packetizePos(QGeoCoordinate pos) // Designed packets (In progress)
+{
+  QByteArray header = QByteArray::number(0000, 4)
+  QByteArray lat = QByteArray::number(pos.latitude(), 'g', 10); // (10 total digits ( 2 integers and 8 decimal points after) equals 33 bytes)
+  lat = lat.rightJustified(10, '0', true);
 
+
+  QByteArray lon = QByteArray::number(pos.longitude(), 'g', 10)
+  lon = lon.rightJustified(10, '0', true);
+  QGeoCoordinate newpos = QGeoCoordinate(lat, lon) // new coordinates
+
+  QByteArray bearing = azimuthTo(QGeoCoordinate newpos & QGeoCoordinate pos) // calculates drone bearing
+  bearing = bearing.rightJustified(3, '0', true); // bearing relative north
+  return header + lat + lon + bearing;
+}
 void UBPacket::depacketize(const QByteArray& packet) {
     m_srcID = *((quint8*)(packet.mid(0, sizeof(quint8)).data()));
     m_desID = *((quint8*)(packet.mid(0 + sizeof(quint8), sizeof(quint8)).data()));
 
     m_payload = packet.mid(0 + sizeof(quint8) + sizeof(quint8));
+QByteArray UBPacket::processpacketsPos(const QByteArray & packet)
+{
+  header = packet.mid(0,4)
+  switch (header)
+
+  case "0000" // read first 4 numbers of our received packet
+    m_mission_state = STATE_IDLE;
+    break;
+  case "0001"
+    break;
+  case "0010"
+    break;
+  case "0100"
+    break;
+  case "1000"
+    break;
+  default:
+    break;
+}
 }
